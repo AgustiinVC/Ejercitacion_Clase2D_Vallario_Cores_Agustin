e5a70152b88681eb35db8fbfc9330132f8e68a22
AgustiinVC <agustiin19@gmail.com>
2022-08-22 23:14:49 -0300

    Ejercitacion Clase 01

---
+++ Clase 01/Ejercicio 01 - Maximos y Minimos/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Ejercicio_01___Maximos_y_Minimos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Consigna
+                Ingresar 5 números por consola, guardándolos en una variable escalar.
+                Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio. */
+
+            int numeroIngresado;
+            int numeroMaximo;
+            int numeroMinimo;
+            int sumaTotalNumeros = 0;
+            float promedioNumeros;
+            bool flag = true;
+
+            for (int contador = 0; contador < 5; contador++)
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese un numero: ");
+                    flag = int.TryParse(Console.ReadLine(), out numeroIngresado);
+                }
+                while (!flag);
+                if (contador == 0)
+                {
+                    numeroMaximo = numeroIngresado;
+                    numeroMinimo = numeroIngresado;
+                }
+                else
+                {
+                    if (numeroIngresado > numeroMaximo)
+                    {
+                        numeroMaximo = numeroIngresado;
+                    }
+                    if (numeroIngresado < numeroMinimo)
+                    {
+                        numeroMinimo = numeroIngresado;
+                    }
+                }
+                sumaTotalNumeros += numeroIngresado;
+            }
+            promedioNumeros = sumaTotalNumeros / 5;
+            Console.WriteLine("El valor maximo es: {0}", numeroMaximo);
+            Console.WriteLine("El valor minimo es: {0}", numeroMinimo);
+            Console.WriteLine("El promedio es: {0}", promedioNumeros);
+
+        }
+    }
+}
---
+++ Clase 01/Ejercicio 02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ejercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Consigna
+                Ingresar un número y mostrar el cuadrado y el cubo del mismo. 
+                Se debe validar que el número sea mayor que cero, caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!". */
+
+            int numeroIngresado;
+            double cuadrado;
+            double cubo;
+            bool flagNumeroIngresado;
+
+            Console.WriteLine("Ingrese un número: ");
+
+            do
+            {
+                flagNumeroIngresado = int.TryParse(Console.ReadLine(), out numeroIngresado);
+                if (!flagNumeroIngresado || numeroIngresado < 1)
+                {
+                    Console.WriteLine("ERROR. ¡Reingresar número!");
+                    flagNumeroIngresado = false;
+                }
+            }
+            while (!flagNumeroIngresado);
+
+            cuadrado = Math.Pow(numeroIngresado, 2);
+            cubo = Math.Pow(numeroIngresado, 3);
+
+            Console.WriteLine("Numero Ingresado: {0} {3}" +
+                                " -- Cuadrado del numero ingresado: {1} {3}" +
+                                " -- Cubo del numero ingresado:     {2}", numeroIngresado, cuadrado, cubo, Environment.NewLine);
+
+        }
+    }
+}
